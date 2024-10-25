3add01eb790004904476339986f9252500f5e578
Riccardo Strina <riccardo.strina@icloud.com>
2024-10-26 01:02:51 +0200

    Update cmake

--- cmake/gauxc-exchcxx.cmake
+++ cmake/gauxc-exchcxx.cmake
@@ -1,35 +1,33 @@
 find_package( ExchCXX QUIET )
 if( NOT ${ExchCXX_FOUND} )
 
-  include( gauxc-dep-versions )
+    include( gauxc-dep-versions )
 
-  message( STATUS "Could not find ExchCXX... Building" )
-  message( STATUS "EXCHCXX REPO = ${GAUXC_EXCHCXX_REPOSITORY}" )
-  message( STATUS "EXCHCXX REV  = ${GAUXC_EXCHCXX_REVISION}"   )
+    message( STATUS "Could not find ExchCXX... Building" )
+    message( STATUS "EXCHCXX REPO = ${GAUXC_EXCHCXX_REPOSITORY}" )
+    message( STATUS "EXCHCXX REV  = ${GAUXC_EXCHCXX_REVISION}"   )
 
-  set( EXCHCXX_ENABLE_CUDA  ${GAUXC_HAS_CUDA} CACHE BOOL "" )
-  set( EXCHCXX_ENABLE_HIP   ${GAUXC_HAS_HIP}  CACHE BOOL "" )
-  set( EXCHCXX_ENABLE_TESTS OFF               CACHE BOOL "" )
+    set( EXCHCXX_ENABLE_CUDA  ${GAUXC_HAS_CUDA} CACHE BOOL "" )
+    set( EXCHCXX_ENABLE_HIP   ${GAUXC_HAS_HIP}  CACHE BOOL "" )
+    set( EXCHCXX_ENABLE_TESTS OFF               CACHE BOOL "" )
 
-  FetchContent_Declare(
+    FetchContent_Declare(
     exchcxx
-    GIT_REPOSITORY ${GAUXC_EXCHCXX_REPOSITORY} 
-    GIT_TAG        ${GAUXC_EXCHCXX_REVISION} 
+    GIT_REPOSITORY ${GAUXC_EXCHCXX_REPOSITORY}
+    # GIT_TAG        ${GAUXC_EXCHCXX_REVISION}
   )
 
-  FetchContent_MakeAvailable( exchcxx )
+    FetchContent_MakeAvailable( exchcxx )
 
 
 else()
 
-  if( ${GAUXC_HAS_CUDA} AND NOT ${EXCHCXX_ENABLE_CUDA} )
-    message( FATAL_ERROR "GauXC CUDA BINDINGS REQUIRE ExchCXX CUDA Bindings" )
-  endif()
+    if( ${GAUXC_HAS_CUDA} AND NOT ${EXCHCXX_ENABLE_CUDA} )
+        message( FATAL_ERROR "GauXC CUDA BINDINGS REQUIRE ExchCXX CUDA Bindings" )
+    endif()
 
-  if( ${GAUXC_HAS_HIP} AND NOT ${EXCHCXX_ENABLE_HIP} )
-    message( FATAL_ERROR "GauXC HIP BINDINGS REQUIRE ExchCXX HIP Bindings" )
-  endif()
+    if( ${GAUXC_HAS_HIP} AND NOT ${EXCHCXX_ENABLE_HIP} )
+        message( FATAL_ERROR "GauXC HIP BINDINGS REQUIRE ExchCXX HIP Bindings" )
+    endif()
 
 endif()
-
-
